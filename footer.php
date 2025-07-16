d4ee345ccd57add639b67e25934253bf42ea0e16
mahshidkhodsiani <mahshidkhodsiani@yahoo.com>
2025-07-16 23:49:55 +0330

    001

---
+++ footer.php
@@ -0,0 +1,41 @@
+<div class="container">
+    <footer class="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top">
+        <div class="col-9 col-md-3 mb-3" >
+            <a href="./" class="">
+                <img src="images/logo.jpeg" class="img-fluid" height="200px" width="200px">
+            </a>
+            <p>Build your academic profile, share your work, and connect with a global network of peers and experts. Join now and start showcasing your research.</p>
+            <p class="text-body-secondary">© 2025</p>
+        </div>
+        <div class="col-3 col-md-2 mb-3"></div>
+
+        <div class="col-4 col-md-2 mb-3">
+            <h5>Section</h5>
+            <ul class="nav flex-column">
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Home</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Features</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Pricing</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">FAQs</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">About</a></li>
+            </ul>
+        </div>
+        <div class="col-4 col-md-2 mb-3">
+            <h5>Section</h5>
+            <ul class="nav flex-column">
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Home</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Features</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Pricing</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">FAQs</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">About</a></li>
+            </ul>
+        </div>
+        <div class="col-4 col-md-2 mb-3">
+            <h5>Download App</h5>
+            <div class="m-2">
+                <img src="images/5.png" class="img-fluid" height="50px" width="50px">
+                <img src="images/4.png" class="img-fluid" height="50px" width="50px">
+            </div>
+
+        </div>
+    </footer>
+</div>
---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>paperet</title>
+
+    <?php include "includes.php"; ?>
+
+</head>
+
+<body>
+
+    <?php include "header.php"; ?>
+
+    <div class="container">
+        <div class="row">
+
+            <div class="col-md-6">
+                <img src="images/2.jpg" class="img-fluid">
+            </div>
+
+            <div class="col-md-6 text-center">
+                <h2>Your Journey Starts Right Here!</h2>
+                <p>Showcase Yourself To The World</p>
+
+                <button class="btn btn-outline-info rounded mb-3">
+                    <i class="fas fa-user-circle"></i> Your Profile
+                </button>
+                <br>
+                <button class="btn btn-outline-info rounded mb-3">
+                    <i class="fab fa-google"></i> Sign in/up With Google
+                </button>
+                <br>
+                <button class="btn btn-outline-info rounded mb-3">
+                    <i class="fas fa-microphone"></i> Request To Present
+                </button>
+                <br>
+                <button class="btn btn-outline-info rounded mb-3">
+                    <i class="fab fa-google"></i> Sign in/up With Google
+                </button>
+                <br>
+                <button class="btn btn-outline-info rounded mb-3">
+                    <i class="fas fa-user-plus"></i> Invite
+                </button>
+                <br>
+                <button class="btn btn-outline-info rounded mb-3">
+                    <i class="fas fa-trophy"></i> Host a Competition
+                </button>
+            </div>
+
+        </div>
+
+        <br />
+
+        <div class="row">
+
+            <div class="col-md-6 text-center">
+                <h6>Paperet</h6>
+                <h2>Take a peek into who we are and what we do</h2>
+                <p>We believe in sharing our journey with you. Learn more about our mission, values, and the passion behind our work.</p>
+            </div>
+
+            <div class="col-md-6">
+                <img src="images/3.png" class="img-fluid">
+            </div>
+        </div>
+    </div>
+
+    <?php include "footer.php"; ?>
+
+</body>
+
+</html>
