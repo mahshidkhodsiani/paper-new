58ad29cd9a891042a188555d36575583166f1539
mahshidkhodsiani <your@email.com>
2025-07-28 16:04:23 +0330

    012-p

--- header.php
+++ header.php
@@ -4,47 +4,6 @@
             <a class="nav-link" href="./">
                 <img src="images\logo.jpeg" alt="paperet" style="height: 30px;">
             </a>
-            <form class="col-12 col-lg-5 mb-3 mb-lg-0 me-lg-3 mr-2" style="margin-left: 5px;">
-                <input type="search" class="form-control" placeholder="Search..." aria-label="Search">
-            </form>
-
-            <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                <li><a href="#" class="nav-link px-2 link-secondary"><i class="fab fa-linkedin"></i> LinkedIn</a></li>
-                <li><a href="people" class="nav-link px-2 link-dark"><i class="fas fa-users"></i> People</a></li>
-
-                <li class="nav-item dropdown">
-                    <a class="nav-link px-2 link-dark dropdown-toggle" href="#" id="navbarDropdownNotification" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        <i class="fas fa-bell"></i> Notification
-                        <span class="badge bg-danger rounded-pill">3</span> </a>
-                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownNotification">
-                        <li><a class="dropdown-item" href="#">You have 3 new messages.</a></li>
-                        <li><a class="dropdown-item" href="#">A new post from John Doe.</a></li>
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
-                        <li><a class="dropdown-item" href="#">View all notifications</a></li>
-                    </ul>
-                </li>
-            </ul>
-
-            <div class="dropdown text-end">
-                <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-                    <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" class="rounded-circle">
-                </a>
-                <ul class="dropdown-menu text-small" aria-labelledby="dropdownUser1">
-                    <li><a class="dropdown-item" href="#">Dashboard...</a></li>
-                    <li><a class="dropdown-item" href="#">Settings</a></li>
-                    <li><a class="dropdown-item" href="#">Profile</a></li>
-                    <li>
-                        <hr class="dropdown-divider">
-                    </li>
-                    <li><a class="dropdown-item" href="#">Sign out</a></li>
-                </ul>
-            </div>
-            </div>
-            </div>
-            </header>
-
         </li>
         <li class="nav-item ms-auto m-1">
             <a class="btn btn-info" href="login">login</a>
